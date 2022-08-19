2d5826bdd4637c4669be94ce58c6a38b7022e8a7
pavan <pavankumar.y.c@greenwayhealth.com>
2022-08-19 16:50:33 +0530

    minor bug fix

--- nodeApp.js
+++ nodeApp.js
@@ -2,13 +2,13 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
        
-    console.log("entered node app");
+    console.log("entered nodeapp");
   
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/plain");
     res.end(JSON.stringify({connection : successfull}));
     });
 
-server.listen(3030, localhost, () => {
+server.listen(3030, "localhost", () => {
   console.log(`Server running at http://${node_hostname}:${node_port}/`);
 });
