e1906d247918a93ce1cc2e18b0461b37ef79b648
pavan <pavankumar.y.c@greenwayhealth.com>
2022-08-22 15:16:46 +0530

    added logs to trigger jenkins build

--- nodeApp.js
+++ nodeApp.js
@@ -2,7 +2,8 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
        
-    console.log("entered nodeapp");
+    console.log("1. entered nodeapp");
+    console.log("2. entered nodeapp");
   
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/plain");
